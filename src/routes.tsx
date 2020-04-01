a2e677d189f33891ed61e3bc7208bf40959d589b
bodi44 <stogrinec.bodia@gmail.com>
2020-04-01 23:30:39 +0300

    fix routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,14 +2,12 @@ import React, { FC, ReactElement } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import HomePage from './pages/home-page/home-page';
 import BookForm from './pages/book-form/book-form';
-import NotFound from './pages/not-found/not-found';
 
 const Routes: FC = (): ReactElement => (
   <Switch>
     <Route exact path={'/'} component={HomePage} />
     <Route path={'/book-form'} component={BookForm} />
-    <Route path={'/not-found'} component={NotFound} />
-    <Redirect to={'/not-found'} />
+    <Redirect to={'/'} />
   </Switch>
 );
 
